905727b1a23c7148094ef18618fe7d2d42e638bd
Matthew Heun <matthew.heun@me.com>
2024-03-13 09:30:13 -0400

    Better compile script

--- compile.sh
+++ compile.sh
@@ -1,44 +1,31 @@
 #!/bin/bash
 
+# Knit the files
 /usr/local/bin/Rscript knit_script.R
 
-source_file_base="HSB_framework"
-destination_file_base="HSB_framework_$(date +'%Y-%m-%d')"
+source_file_base_framework="HSB_framework"
+destination_file_base_framework="HSB_framework_$(date +'%Y-%m-%d')"
+source_file_base_results="HSB_results"
+destination_file_base_results="HSB_results_$(date +'%Y-%m-%d')"
 
-# echo "File copy from $source_file_base.pdf started"
-
-# Copy the file with the new name
-cp "$source_file_base.pdf" "$destination_file_base.pdf"
-
-# echo "File copied successfully to $destination_file_base.pdf"
-
-# echo "Started latexdiff"
-
+# Copy the .pdf file to a different name with date appended
+cp "$source_file_base_framework.pdf" "$destination_file_base_framework.pdf"
+cp "$source_file_base_results.pdf" "$destination_file_base_results.pdf"
 # Run the difference command
-latexdiff "$source_file_base-initial-submission.tex" "$source_file_base.tex" > diff_framework.tex
+latexdiff "$source_file_base_framework-initial-submission.tex" "$source_file_base_framework.tex" > "diff_$destination_file_base_framework.tex"
+latexdiff "$source_file_base_results-initial-submission.tex" "$source_file_base_results.tex" > "diff_$destination_file_base_results.tex"
+# Compile the difference file
+pdflatex "diff_$destination_file_base_framework.tex"
+bibtex "diff_$destination_file_base_framework"
+pdflatex "diff_$destination_file_base_framework.tex"
+pdflatex "diff_$destination_file_base_framework.tex"
 
-# echo "Finished latexdiff"
+pdflatex "diff_$destination_file_base_results.tex"
+bibtex "diff_$destination_file_base_results"
+pdflatex "diff_$destination_file_base_results.tex"
+pdflatex "diff_$destination_file_base_results.tex"
 
-# echo "Started compiling diff.tex"
-
-pdflatex diff_framework.tex
-open "$destination_file_base.pdf"
-open diff_framework.pdf
-
-
-# echo "Completed compiling diff.tex"
-
-source_file_base="HSB_results"
-destination_file_base="HSB_results_$(date +'%Y-%m-%d')"
-
-cp "$source_file_base.pdf" "$destination_file_base.pdf"
-latexdiff "$source_file_base-initial-submission.tex" "$source_file_base.tex" > diff_results.tex
-pdflatex diff_results.tex
-bibtex diff_results
-pdflatex diff_results.tex
-pdflatex diff_results.tex
-
-
-open "$destination_file_base.pdf"
-
-open diff_results.pdf
+open "$destination_file_base_framework.pdf"
+open "$destination_file_base_results.pdf"
+open "diff_$destination_file_base_framework.pdf"
+open "diff_$destination_file_base_results.pdf"
